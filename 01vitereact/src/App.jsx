ef05d47db6b7045fb8dfad7238cd1872b649a7db
PITPL-HITESH-PANCHANI <hitesh@pseudocode.co>
2023-12-28 12:37:04 +0530

    first commit

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1>Chai or react with vite</h1>
+    </>
+  )
+}
+
+export default App
--- Readme.md
+++ Readme.md
@@ -1,1 +1,2 @@
 test
+https://vscode.dev/tunnel/hitesh-pc/home/hitesh/Desktop/my-tutorial/hitesh-chaudhary/react/chai-aur-react
